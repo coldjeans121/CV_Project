16e6ba7300f6bdb64a1740152b5cfe60ecbc1570
Home <CJ>
2020-10-26 03:01:28 +0900

    cv_project1 html

--- Img2txt/Img2txt.py
+++ Img2txt/Img2txt.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import ImageFont, ImageDraw, Image
 
 
-def main(img, c0='ㄱ', c1='ㄴ'):
+def main(img, c0='A', c1='B'):
     txt_dict = {255: c0, 0: c1}
     h, w = img.shape
     txt = ""
@@ -11,7 +11,8 @@ def main(img, c0='ㄱ', c1='ㄴ'):
         for i in range(w):
             txt += txt_dict[img[j, i]]
         txt += "\n"
-    print(txt)
+    # print(txt)
+    return txt
 
 
 def img_loader():
@@ -19,10 +20,14 @@ def img_loader():
     return cv2.imread(img_root)
 
 
-def letter2img(height=15):
-    letter = "테스트? Test!"
+def letter2img(str_input="", height=15, c0='A', c1='B'):
+    return main(_letter2img(str_input, height), c0, c1)
+
+
+def _letter2img(str_input="나랑", height=15):
+    letter = str_input
     length = len(letter)
-    sketch_img = np.zeros((20, 20*length), dtype=np.uint8)
+    sketch_img = np.zeros((20, 30*length), dtype=np.uint8)
     # sketch_img = cv2.putText(sketch_img, letter, (0, 12), 1, 1, 255, 1)
 
     # put text in null image
@@ -34,25 +39,24 @@ def letter2img(height=15):
     sketch_img = np.array(img_pil)
 
     cv2.imshow("test1", sketch_img)
-    # height resize
-    sketch_img = cv2.resize(sketch_img, (20*length, height))
-    # margin cut resize
-    for i in range(1, 20*length):
+    # resize height
+    sketch_img = cv2.resize(sketch_img, (30*length, height))
+    # cut margin
+    for i in range(1, 30*length):
         if np.sum(sketch_img[:, -i]):
             sketch_img = sketch_img[:, :-i+2]
             break
     # binarization
-
     cv2.imshow("test2", sketch_img)
-    sketch_img[sketch_img >= 100] = 255
-    sketch_img[sketch_img < 100] = 0
+    sketch_img[sketch_img >= 50] = 255
+    sketch_img[sketch_img < 50] = 0
 
     cv2.imshow("test", sketch_img)
-    cv2.waitKey(0)
+    # cv2.waitKey(0)
     return sketch_img
 
 
 if __name__ == '__main__':
-    image = letter2img()
+    image = _letter2img()
     main(image)
 
---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, url_for
+from Img2txt.Img2txt import letter2img
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main(num=None):
+    return "https://real-cold-river.tistory.com/"
+
+
+@app.route('/cv_project1')
+def cv_project1(num=None):
+    return render_template('Img2txt.html', num=num)
+
+
+@app.route('/cv_project1_result', methods=['POST', 'GET'])
+def cv_project1_alg(num=None):
+    if request.method == 'POST':
+        pass
+    elif request.method == 'GET':
+        char_input = request.args.get('char_input')
+        char_input = "Test" if char_input == '' else char_input
+        temp = request.args.get('num')
+        try:
+            temp = int(temp)
+        except ValueError:
+            temp = 15
+        c0, c1 = request.args.get('c0'), request.args.get('c1')
+        c0 = '6' if c0 == '' else c0
+        c1 = '9' if c1 == '' else c1
+        print("Test", char_input, temp, c0, c1)
+        char_result = letter2img(char_input, temp, c0, c1)
+        print(char_result)
+        # char_result = char_result.replace("\n", "<br>")
+        return render_template('Img2txt.html', num=temp, char_result=char_result.split("\n"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
